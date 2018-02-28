8cc85b682d2b8d629c049baea1bff0d972845edd
Stefano Fadda <stefano@crs4.it>
2018-02-28 11:32:01 +0100

    added versioning feature

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Value = require('./lib/value');
 const Text = require('./lib/text');
 const Data = require('./lib/data');
 const Node = require('./lib/node');
+const Authoring = require('./lib/authoring');
 
 module.exports = {
   Client,
@@ -13,5 +14,6 @@ module.exports = {
   Value,
   Text,
   Data,
-  Node
+  Node,
+  Authoring
 };
